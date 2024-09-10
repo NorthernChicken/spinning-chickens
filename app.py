5df610b39b0ce40a7e011df32fce27316a8d434c
AJ Wu <aj.joshuawu@gmail.com>
2024-09-09 17:33:35 -0700

    update

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, Markup
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Path to the notes file
 NOTES_FILE = 'notes.txt'
-
-# Ensure the notes file exists
 if not os.path.exists(NOTES_FILE):
     open(NOTES_FILE, 'w').close()
 
-# Route for the home page
 @app.route('/')
 def index():
     with open(NOTES_FILE, 'r') as file:
@@ -19,11 +16,20 @@ def index():
 
 # Route for submitting a note
 @app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['POST'])
 def submit():
-    note = request.form['note']
+    current_datetime = datetime.now()
+    formatted_datetime = f"<i>{current_datetime.strftime('%Y-%m-%d %H:%M:%S')}</i>"
+    note = f"<b>{request.form['note']}</b>"
+    sign = request.form['sign'].strip()
+    if not sign:
+        signature = "<i>Anonymous</i>"
+    else:
+        signature = f"<i>{sign}</i>"
     with open(NOTES_FILE, 'a') as file:
-        file.write(note + '\n')
+        file.write(note + " - " + signature + ", " + formatted_datetime + '\n')
     return redirect('/')
+
 
 @app.route('/about')
 def about():
@@ -37,6 +43,5 @@ def contact():
 def links():
     return render_template('links.html')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
