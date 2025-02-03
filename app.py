81528d59ffbcbd743ad5b9630f2c20f812559cf6
NorthernChicken <ajjoshuawu@gmail.com>
2025-02-03 05:35:35 +0000

    changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect, render_template, Markup
+from flask import Flask, request, redirect, render_template
+from markupsafe import Markup
 import os
 from datetime import datetime
 
